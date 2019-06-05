8059b43026aad5a4ff013d4769b7617c24ec0ef5
Byron Varberg <byron@crema.us>
2019-06-05 16:30:16 -0500

    Highlight out of stock products
    This was included in the guide, but I didn't implement it myself until
    looking through the example for the ProductRow component.

--- src/components/ProductRow/index.tsx
+++ src/components/ProductRow/index.tsx
@@ -4,9 +4,15 @@ import { Product } from "../../types/Product"
 type Props = Readonly<{ product: Product }>
 
 export function ProductRow({ product }: Props) {
+  const name: string | React.ReactElement = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  )
+
   return (
     <tr className="ProductRow">
-      <td>{product.name}</td>
+      <td>{name}</td>
       <td>{product.price}</td>
     </tr>
   )
